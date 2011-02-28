66fd6f1cf62c66b03c19bdda8cc0ee82a5165958
Igal Koshevoy <igal@pragmaticraft.com>
2011-02-28 03:16:36 -0800

    Silenced unnecessary, whiny scaffolded tests.

--- spec/dummy/spec/helpers/entities_helper_spec.rb
+++ spec/dummy/spec/helpers/entities_helper_spec.rb
@@ -11,5 +11,5 @@ require 'spec_helper'
 #   end
 # end
 describe EntitiesHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #IK# pending "add some examples to (or delete) #{__FILE__}"
 end
