8247d02dc5d478511ccac7b10324de88fce31303
Igal Koshevoy <igal@pragmaticraft.com>
2011-02-28 02:52:08 -0800

    MIGRATION: Add 'paper_trail' table.

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110228093241) do
+ActiveRecord::Schema.define(:version => 20110228094444) do
 
   create_table "entities", :force => true do |t|
     t.string   "name"
@@ -25,5 +25,16 @@ ActiveRecord::Schema.define(:version => 20110228093241) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "versions", :force => true do |t|
+    t.string   "item_type",  :null => false
+    t.integer  "item_id",    :null => false
+    t.string   "event",      :null => false
+    t.string   "whodunnit"
+    t.text     "object"
+    t.datetime "created_at"
+  end
+
+  add_index "versions", ["item_type", "item_id"], :name => "index_versions_on_item_type_and_item_id"
 
 end
