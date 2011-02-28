e0cbae53ae41811458db57d8149722b411369af7
Igal Koshevoy <igal@pragmaticraft.com>
2011-02-28 02:36:48 -0800

    Add 'paper_trail' versioning to dummy models.

--- spec/dummy/app/models/entity.rb
+++ spec/dummy/app/models/entity.rb
@@ -1,4 +1,6 @@
 class Entity < ActiveRecord::Base
+  has_paper_trail
+
   validates_presence_of :name
   validates_presence_of :status
 end
--- spec/dummy/app/models/platform.rb
+++ spec/dummy/app/models/platform.rb
@@ -1,4 +1,6 @@
 class Platform < ActiveRecord::Base
+  has_paper_trail
+
   validates_presence_of :name
   validates_presence_of :status
 end
