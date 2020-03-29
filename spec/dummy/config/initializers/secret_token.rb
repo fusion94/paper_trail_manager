3fc282193334a735a880da1e8f867544d6935d7a
Reid Beels <mail@reidbeels.com>
2020-03-29 14:05:41 -0700

    Remove spec/dummy from git

--- spec/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Rails328::Application.config.secret_key_base = Rails328::Application.config.secret_token = '96169e00f121ca10adb3df3e38edd800c2b0972dc6c9054a9e124979643558199e4245eb3bd60d0fb0f6e71734cc170ce4b57721e55fa106221cda73c9281c5e'
